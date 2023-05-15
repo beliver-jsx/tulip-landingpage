b503a3986c8f1c4fce919e3ceb1a290205151d6f
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-05-16 01:15:52 +0530

    TextRevealAnimation

--- src/components/Animation/TextRevealAnimation.tsx
+++ src/components/Animation/TextRevealAnimation.tsx
@@ -6,15 +6,15 @@ import { motion } from "framer-motion";
 
 const TextRevealAnimation: React.FC<props> = ({ lines, classNames }) => {
     const list = lines.map((line, index) => (
-        <motion.div className='overflow-y-hidden'>
+        <motion.div className='overflow-y-hidden '>
             <motion.div
                 key={index}
                 animate={{ y: "0%", opacity: 1 }}
-                initial={{ y: "100%", opacity: 0 }}
+                initial={{ y: "90%", opacity: 0 }}
                 transition={{
-                    duration: 1,
+                    duration: .8,
                     ease: [1, 1, .70, .90],
-                    delay: index == 0 ? 0 : 0.2 * index,
+                    delay: index == 0 ? 0 : 0.1 * index,
                 }}>
                 <h1 className="4xl:text-xl 3xl:text-[112px] 2xl:text-[112px] xl:text-[96px] lg:text-[96px] md:text-[80px] text-[52px] font-[800]">{line}</h1>
             </motion.div>
