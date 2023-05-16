db144b39a8b5df6bb5b4cbb1869cc9ddf23a4c21
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-05-16 15:17:11 +0530

    ...

--- src/components/Animation/TextRevealAnimation.tsx
+++ src/components/Animation/TextRevealAnimation.tsx
@@ -5,31 +5,10 @@ interface props {
 import { motion } from "framer-motion";
 
 const TextRevealAnimation: React.FC<props> = ({ lines, classNames }) => {
-    const list = lines.map((line, index) => (
-        <motion.div className='overflow-y-hidden '>
-            <motion.div
-                key={index}
-                animate={{ y: "0%", opacity: 1 }}
-                initial={{ y: "50%", opacity: 0 }}
-                transition={{
-                    duration: index == 0 ? .5 : 1,
-                    ease: [0.08, 0.82, 0.17, 1],
-                    delay: index == 0 ? 0 : 0.1 * index,
-                }}>
-                <h1 className="4xl:text-xl 3xl:text-[112px] 2xl:text-[112px] xl:text-[96px] lg:text-[96px] md:text-[80px] text-[52px] font-[800]">{line}</h1>
-            </motion.div>
-        </motion.div>
-    ))
     return (
-        // <motion.div
-        //     className={classNames}
-        //     transition={{ staggerChildren: 0.5 }}>
-        //     {list}
-        // </motion.div >
-
-
-
-        <motion.div>
+        <motion.div
+            className={classNames}
+        >
 
             {
                 lines.map((line, index) => (
@@ -39,9 +18,8 @@ const TextRevealAnimation: React.FC<props> = ({ lines, classNames }) => {
                             animate={{
                                 y: 0, opacity: 1, transition: {
                                     delay: index == 0 ? 0 : (index + 0.2) * 0.1,
-                                    duration: .5,
                                     type: 'spring',
-                                    stiffness: 300,
+                                    stiffness: 80,
                                     damping: 25,
                                 }
                             }}
