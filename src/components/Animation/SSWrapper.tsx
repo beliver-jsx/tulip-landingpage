d1ad2222af50a87253222f53b9dede7be25cd7cc
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-05-16 15:24:56 +0530

    Update SSWrapper.tsx

--- src/components/Animation/SSWrapper.tsx
+++ src/components/Animation/SSWrapper.tsx
@@ -1,16 +1,9 @@
-import React, { useRef, useState, useEffect, useCallback, useLayoutEffect } from "react"
-import ResizeObserver from "resize-observer-polyfill"
-import {
-    useTransform,
-    useSpring,
-    motion,
-    useScroll
-} from "framer-motion"
-
 import Skew from "../Skew"
-
 import { useAppSelector } from "@/store/hooks"
+import ResizeObserver from "resize-observer-polyfill"
 import useMobileDetect from "@/hooks/useMobileDetect"
+import { useTransform, useSpring, motion, useScroll } from "framer-motion"
+import React, { useRef, useState, useEffect, useCallback, useLayoutEffect } from "react"
 
 const SmoothScroll = ({ children }: any) => {
     // scroll container
@@ -40,10 +33,8 @@ const SmoothScroll = ({ children }: any) => {
     // as scrollY changes between 0px and the scrollable height, create a negative scroll value...
     // ... based on current scroll position to translateY the document in a natural way
     const transform = useTransform(scrollY, [0, pageHeight], [0, -pageHeight])
-    const physics = { damping: 80, stiffness: 200, } // easing of smooth scroll
+    const physics = { damping: 80, stiffness: 200, }  // easing of smooth scroll
     const spring = useSpring(transform, physics) // apply easing to the negative scroll value
-
-
 
 
     // Cursor Placement & Animation
@@ -105,7 +96,7 @@ const SmoothScroll = ({ children }: any) => {
 
             <motion.div
                 ref={scrollRef}
-                style={{ y: spring }} // translateY of scroll container using negative scroll value
+                style={{ y: isMobile ? transform : spring }} // translateY of scroll container using negative scroll value
                 className="fixed 4xl:w-[calc(100%-600px)] 3xl:w-[calc(100%-500px)] 2xl:w-[calc(100%-400px)] xl:w-[calc(100%-352px)] lg:w-[calc(100%-96px)] md:w-[calc(100%-96px)] w-[calc(100%-48px)] 4xl:left-[300px] 3xl:left-[250px] 2xl:left-[200px] xl:left-[176px] lg:left-[48px] md:left-[48px] left-[24px] top-0 h-max "
             >
                 <Skew>
