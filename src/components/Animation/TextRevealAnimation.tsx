bd7d4305a714201e41145dc8e4c80e2658c346ba
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-05-18 00:34:08 +0530

    Update TextRevealAnimation.tsx

--- src/components/Animation/TextRevealAnimation.tsx
+++ src/components/Animation/TextRevealAnimation.tsx
@@ -21,7 +21,11 @@ const TextRevealAnimation: React.FC<props> = ({ lines, classNames }) => {
                                     stiffness: 1000,
                                     damping: 100,
                                 }
-                            }}>
+                            }}
+                            initial={{ opacity: 1 }}
+
+
+                        >
                             <h1 className="4xl:text-xl 3xl:text-[112px] 2xl:text-[112px] xl:text-[96px] lg:text-[96px] md:text-[80px] text-[52px] font-[800]">{line}</h1>
                         </motion.div>
 
