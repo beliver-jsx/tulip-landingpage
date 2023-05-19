4257070bb5d26fcb8418a2ecff288debe7567378
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-05-19 11:22:53 +0530

    Update BackToTop.tsx

--- src/components/atoms/BackToTop.tsx
+++ src/components/atoms/BackToTop.tsx
@@ -7,7 +7,7 @@ const BackToTop = () => {
         });
     };
     return (
-        <div className="mt-[15rem]">
+        <div className=" flex items-center justify-center flex-col h-screen border-2 border-transparent  ">
             <OutlineText onClick={scrollToTop} text="Back to top" classNames="4xl:text-[12rem]  3xl:text-[112px] 2xl:text-[112px] xl:text-[96px] lg:text-[96px] md:text-[80px] text-[52px] font-[800] text-center cursor-pointer w-full" />
             <p className="text-center text-sm text-gray"> ©️2023 - Richard William</p>
             <p className="text-center text-sm text-gray mt-2 mb-10">Made with ❣️ by Deepak Vishwakarma</p>
