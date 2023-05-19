9ca97b9aee3c8ceb7d68d556c5075810ae604ab4
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-05-19 12:29:50 +0530

    back to top is full responsive

--- src/components/atoms/BackToTop.tsx
+++ src/components/atoms/BackToTop.tsx
@@ -8,7 +8,7 @@ const BackToTop = () => {
     };
     return (
         <div className=" flex items-center justify-center flex-col h-screen border-2 border-transparent  ">
-            <OutlineText onClick={scrollToTop} text="Back to top" classNames="4xl:text-[13rem] font-[1000]  3xl:text-[112px] 2xl:text-[112px] xl:text-[96px] lg:text-[96px] md:text-[80px] text-[52px]  text-center cursor-pointer w-full" />
+            <OutlineText onClick={scrollToTop} text="Back to top" classNames=" font-[1000] 4xl:text-[13rem]   3xl:text-[10rem] 2xl:text-[10rem] xl:text-[9rem] lg:text-[9rem] md:text-[5rem] text-[5rem]  text-center cursor-pointer w-full" />
             <p className="text-center text-sm text-gray"> ©️2023 - Richard William</p>
             <p className="text-center text-sm text-gray mt-2 mb-10">Made with ❣️ by Deepak Vishwakarma</p>
             <p className="text-center text-sm text-gray">Thanks for scrolling!</p>
