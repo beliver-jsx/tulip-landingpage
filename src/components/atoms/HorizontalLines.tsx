49d230e65befa8fdfcd551974f84ed7ecc479345
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-05-19 12:14:26 +0530

    smooth transition on theme switch

--- src/components/atoms/HorizontalLines.tsx
+++ src/components/atoms/HorizontalLines.tsx
@@ -1,8 +1,15 @@
+// smmoth aniamation while theme change 
+import { motion } from 'framer-motion'
+
 const HorizontalLine = () => (
-    <div className="grid grid-cols-[1fr_1fr_1fr] w-full h-screen top-0 4xl:px-[300px] 3xl:px-[250px] 2xl:px-[200px] xl:px-[176px] lg:px-[48px] md:px-[48px] sm:px-[48px] fixed ">
-        <div className="border-l border-[#ececec] dark:border-[#ececec1b]">&nbsp;</div>
-        <div className="border-l border-[#ececec] dark:border-[#ececec1b]">&nbsp;</div>
-        <div className="border border-y-0 border-[#ececec] dark:border-[#ececec1b]">&nbsp;</div>
+    <div className=" transition-colors duration-1000 grid grid-cols-[1fr_1fr_1fr] w-full h-screen top-0 4xl:px-[300px] 3xl:px-[250px] 2xl:px-[200px] xl:px-[176px] lg:px-[48px] md:px-[48px] sm:px-[48px] fixed  ">
+
+        <motion.div className="border-l border-t-0 border-b-0 border-r-0  border-2 border-[#00000010]">&nbsp;</motion.div>
+
+        <motion.div className="border-l border-t-0 border-b-0 border-r-0  border-2 border-[#00000010]">&nbsp;</motion.div>
+
+        <motion.div className="border-l border-t-0 border-b-0 border-r border-2 border-[#00000010]">&nbsp;</motion.div>
+
     </div>
 )
 
